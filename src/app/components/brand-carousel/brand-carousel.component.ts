cb7003901135aa9d0eb7d4fba725ed9ff022f870
mixxr <ms.co.it@gmail.com>
2017-01-03 19:57:22 +0100

    cambio sfondo e menu

--- src/app/components/brand-carousel/brand-carousel.component.ts
+++ src/app/components/brand-carousel/brand-carousel.component.ts
@@ -9,17 +9,51 @@ import {Brand} from '../../model/brand';
 export class BrandCarouselComponent implements OnInit {
 
   list:Brand[] = [];
+  maxActive: number;
+  currentPos:number;
   constructor() { 
+    this.maxActive = 7;
+    this.currentPos = 0;
     this.list = [
       new Brand("Gucci","gucci","/assets/images/brands/gucci.jpg"),
       new Brand("Cartier","cartier","/assets/images/brands/cartier.jpg"),
       new Brand("hermes_paris","hermes_paris","/assets/images/brands/hermes_paris.jpg"),
       new Brand("louis_vuitton","louis_vuitton","/assets/images/brands/louis_vuitton.jpg"),
+      new Brand("prada","prada","/assets/images/brands/prada.jpg"),
+      new Brand("Gucci","gucci","/assets/images/brands/gucci.jpg"),
+      new Brand("Cartier","cartier","/assets/images/brands/cartier.jpg"),
+      new Brand("hermes_paris","hermes_paris","/assets/images/brands/hermes_paris.jpg"),
+      new Brand("louis_vuitton","louis_vuitton","/assets/images/brands/louis_vuitton.jpg"),
+      new Brand("prada","prada","/assets/images/brands/prada.jpg"),
+      new Brand("Gucci","gucci","/assets/images/brands/gucci.jpg"),
+      new Brand("Cartier","cartier","/assets/images/brands/cartier.jpg"),
+      new Brand("hermes_paris","hermes_paris","/assets/images/brands/hermes_paris.jpg"),
+      new Brand("louis_vuitton","louis_vuitton","/assets/images/brands/louis_vuitton.jpg"),
+      new Brand("prada","prada","/assets/images/brands/prada.jpg"),
+            new Brand("Gucci","gucci","/assets/images/brands/gucci.jpg"),
+      new Brand("Cartier","cartier","/assets/images/brands/cartier.jpg"),
+      new Brand("hermes_paris","hermes_paris","/assets/images/brands/hermes_paris.jpg"),
+      new Brand("louis_vuitton","louis_vuitton","/assets/images/brands/louis_vuitton.jpg"),
       new Brand("prada","prada","/assets/images/brands/prada.jpg")
-    ];
+                ];
+
+    // carousel timer
+   //  setInterval(() => { this.moveRight(1); }, 5000);
   }
 
   ngOnInit() {
+  }
+
+  isActive(index: number): boolean{
+    return (index>=0 && index<this.maxActive);
+  }
+
+  display(index: number):string{
+    return this.isActive(index)?"":"none";
+  }
+
+  moveRight(steps:number){
+    this.list = [...this.list.slice(steps), ...this.list.slice(0,steps)];
   }
 
 }
--- src/app/routes/pages/search-page/search-page.component.ts
+++ src/app/routes/pages/search-page/search-page.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-search-page',
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './search-page.component.html',
   styleUrls: ['./search-page.component.css']
 })
